9cb0891e07b51f44aa03a7cf1e884245f059d67f
schallakuchen <andreas.schaller7@gmail.com>
2022-12-06 19:43:37 +0100

    Initial commit

---
+++ src/main/java/at/fhtechnikum/exam_converter/ExamConverterApplication.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.exam_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/fhtechnikum/exam_converter/ExamConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtechnikum.exam_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
