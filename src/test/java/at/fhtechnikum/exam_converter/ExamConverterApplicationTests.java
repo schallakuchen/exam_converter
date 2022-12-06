2b22416fb31f39df9350f6d2d79b8673b7188176
schallakuchen <andreas.schaller7@gmail.com>
2022-12-06 20:22:52 +0100

    #4 Added unit tests for inches to centimeters and yards to meters conversion

--- src/test/java/at/fhtechnikum/exam_converter/ExamConverterApplicationTests.java
+++ src/test/java/at/fhtechnikum/exam_converter/ExamConverterApplicationTests.java
@@ -1,13 +1,27 @@
 package at.fhtechnikum.exam_converter;
 
+import at.fhtechnikum.exam_converter.Controller.MetricConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ExamConverterApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Given
+		MetricConverter controller = new MetricConverter();
+
+		// When
+		double result_centimeter = controller.convertInchToCentimeter(1);
+		double result_meters = controller.convertYardToMeter(1);
+
+		// Then
+		assertEquals(result_centimeter, 2.54);
+		assertEquals(result_meters, 0.9140767824497257);
+
 	}
 
 }
