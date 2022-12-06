9976a64371ee2195fb255ab7c2d99bbd6a67baa2
schallakuchen <andreas.schaller7@gmail.com>
2022-12-06 19:54:13 +0100

    #1 add empty controller

---
+++ src/main/java/at/fhtechnikum/exam_converter/Controller/MetricConverter.java
@@ -0,0 +1,10 @@
+package at.fhtechnikum.exam_converter.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MetricConverter {
+
+}
